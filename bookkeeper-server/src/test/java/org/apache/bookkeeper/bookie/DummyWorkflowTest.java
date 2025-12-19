8f7e60987294df5600be3122456038498713e3e4
shenron221B <120735073+shenron221B@users.noreply.github.com>
2025-12-19 16:24:32 +0100

    CI setup: aggiunti workflow Maven, JaCoCo e Pit + profili

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/DummyWorkflowTest.java
@@ -0,0 +1,13 @@
+package org.apache.bookkeeper.bookie;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class DummyWorkflowTest {
+
+    @Test
+    public void testWorkflowCheck() {
+        System.out.println("test dummy in esecuzione");
+        assertTrue("Questo test deve sempre passare", true);
+    }
+}
